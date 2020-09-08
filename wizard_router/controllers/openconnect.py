92acf4e53fca4d55b51e29de05f1eb5f4c6e5359
mohammad sheikhian <mohammadsheikhian70@gmail.com>
2020-09-08 23:37:21 +0430

    Revise the VPN APIs

--- wizard_router/controllers/openconnect.py
+++ wizard_router/controllers/openconnect.py
@@ -1,3 +1,5 @@
+import os
+
 from nanohttp import RestController, json
 
 
@@ -5,22 +7,72 @@ class OpenConnectController(RestController):
 
     @json(prevent_form=True)
     def status(self):
-        open_connect= dict(
+        open_connect = dict(
             status='active',
+            returnCode=0,
+            exceptionMessage=''
         )
+
+        try:
+            status = os.system(
+                'systemctl is-active --quiet mohammad-openconnect'
+            )
+            open_connect['returnCode'] = status
+            open_connect['status'] = 'active' if status == 0 else 'in-active'
+
+        except Exception as exp:
+            open_connect['returnCode'] = -1
+            open_connect['status'] = 'unknown'
+            open_connect['exceptionMessage'] = exp
+
         return open_connect
 
     @json(prevent_form=True)
-    def reset(self):
+    def restart(self):
         open_connect = dict(
             status='active',
+            returnCode=0,
+            exceptionMessage=''
         )
+
+        try:
+            status = os.system(
+                'systemctl restart --quiet mohammad-openconnect'
+            )
+            open_connect['returnCode'] = status
+
+            status = os.system(
+                'systemctl is-active --quiet mohammad-openconnect'
+            )
+            open_connect['status'] = 'active' if status == 0 else 'in-active'
+
+        except Exception as exp:
+            open_connect['returnCode'] = -1
+            open_connect['status'] = 'unknown'
+            open_connect['exceptionMessage'] = exp
+
         return open_connect
 
     @json(prevent_form=True)
     def stop(self):
         open_connect = dict(
-            status='in-active',
+            status='active',
+            returnCode=0,
+            exceptionMessage=''
         )
-        return open_connect
 
+        try:
+            status = os.system('systemctl stop --quiet mohammad-openconnect')
+            open_connect['returnCode'] = status
+
+            status = os.system(
+                'systemctl is-active --quiet mohammad-openconnect'
+            )
+            open_connect['status'] = 'active' if status == 0 else 'in-active'
+
+        except Exception as exp:
+            open_connect['returnCode'] = -1
+            open_connect['status'] = 'unknown'
+            open_connect['exceptionMessage'] = exp
+
+        return open_connect
